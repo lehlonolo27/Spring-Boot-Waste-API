e9a4854a7a72c6b8ffff8896471548d4244307a9
lehlonolo27 <126181969+lehlonolo27@users.noreply.github.com>
2025-02-05 17:11:39 +0200

    Created WasteCategory Repo

--- waste-sorting-api/src/main/java/com/enviro/assessment/grad001/lehlonolomokoena/waste_sorting_api/repository/WasteCategoryRepository.java
+++ waste-sorting-api/src/main/java/com/enviro/assessment/grad001/lehlonolomokoena/waste_sorting_api/repository/WasteCategoryRepository.java
@@ -1,5 +1,8 @@
 package com.enviro.assessment.grad001.lehlonolomokoena.waste_sorting_api.repository;
+import com.enviro.assessment.grad001.lehlonolomokoena.waste_sorting_api.model.WasteCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class WasteCategoryRepository {
-
+@Repository
+public interface WasteCategoryRepository extends JpaRepository<WasteCategory, Long> {
 }
