2e4d0737f1a0a374c9578e0a21ca2868c61223ae
lehlonolo27 <126181969+lehlonolo27@users.noreply.github.com>
2025-02-05 17:02:36 +0200

    Created Data Transfer Object

--- waste-sorting-api/src/main/java/com/enviro/assessment/grad001/lehlonolomokoena/waste_sorting_api/dto/WasteCategoryDTO.java
+++ waste-sorting-api/src/main/java/com/enviro/assessment/grad001/lehlonolomokoena/waste_sorting_api/dto/WasteCategoryDTO.java
@@ -1,5 +1,18 @@
 package com.enviro.assessment.grad001.lehlonolomokoena.waste_sorting_api.dto;
 
-public class WasteCategoryDTO {
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
+
+public class WasteCategoryDTO {
+    
+    @NotBlank(message = "Category name is required")
+    private String name;
+
+    @NotBlank(message = "Disposal guidelines are required")
+    private String disposalGuidelines;
+
+    @NotBlank(message = "Recycling tips are required")
+    private String recyclingTips;
 }
