9d6cd195da440113d0aaa3e006512c8235674c03
lehlonolo27 <126181969+lehlonolo27@users.noreply.github.com>
2025-02-05 17:00:19 +0200

    Created Model Entity

--- waste-sorting-api/src/main/java/com/enviro/assessment/grad001/lehlonolomokoena/waste_sorting_api/model/WasteCategory.java
+++ waste-sorting-api/src/main/java/com/enviro/assessment/grad001/lehlonolomokoena/waste_sorting_api/model/WasteCategory.java
@@ -1,5 +1,27 @@
 package com.enviro.assessment.grad001.lehlonolomokoena.waste_sorting_api.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "waste_categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class WasteCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @NotBlank(message = "Category name is required")
+    private String name;
+
+    @NotBlank(message = "Disposal guidelines are required")
+    private String disposalGuidelines;
+
+    @NotBlank(message = "Recycling tips are required")
+    private String recyclingTips;
 }
